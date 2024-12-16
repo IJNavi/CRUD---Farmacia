d1e1a63166930e7ab62f6bd6c46b5b7b32353fec
Ivan Barbosa <ivanjulio.ij@gmail.com>
2024-12-16 16:28:17 -0300

    Projeto em andamento.

--- CRUD-Farmacia/src/main/java/com/generation/farmacia/model/Categoria.java
+++ CRUD-Farmacia/src/main/java/com/generation/farmacia/model/Categoria.java
@@ -1,9 +1,16 @@
 package com.generation.farmacia.model;
 
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
@@ -19,6 +26,10 @@ public class Categoria {
 	@Size(min = 4, max = 100, message = "minimo de 4 caracteres e maximo de 100")
 	@NotBlank(message = "O Atributo indicação é obrigatório")
 	private String indicacao;
+	
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "categoria", cascade = CascadeType.REMOVE)
+	@JsonIgnoreProperties("categoria")
+	private List<Produto> produto;
 
 	public Long getId() {
 		return id;
---
+++ CRUD-Farmacia/src/main/java/com/generation/farmacia/model/Produto.java
@@ -0,0 +1,101 @@
+package com.generation.farmacia.model;
+
+import java.time.LocalDate;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+public class Produto {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@NotBlank(message = "O Atributo nome é obrigatório.")
+	@Size(min = 4, max = 100, message = "minimo de 4 caracteres e maximo de 100")
+	private String nome;
+	
+	@NotNull(message = "O Atributo estocado é obrigatório, informa a quantidade em estoque.")
+	private Long estocado;
+	
+	@NotNull(message = "O Atributo vendas realizadas é obrigatório.")
+	private Long vendas;
+	
+	@NotNull(message = "O Atributo vencimento é obrigatório.")
+	private LocalDate vencimento;
+	
+	@NotNull(message = "O Atributo lote é obrigatório.")
+	private Long lote;
+	
+	@NotNull
+	private Boolean generico;
+	
+	@ManyToOne
+	@JsonIgnoreProperties("produto")
+	private Categoria categoria;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Long getEstocado() {
+		return estocado;
+	}
+
+	public void setEstocado(Long estocado) {
+		this.estocado = estocado;
+	}
+
+	public Long getVendas() {
+		return vendas;
+	}
+
+	public void setVendas(Long vendas) {
+		this.vendas = vendas;
+	}
+
+	public LocalDate getVencimento() {
+		return vencimento;
+	}
+
+	public void setVencimento(LocalDate vencimento) {
+		this.vencimento = vencimento;
+	}
+
+	public Long getLote() {
+		return lote;
+	}
+
+	public void setLote(Long lote) {
+		this.lote = lote;
+	}
+
+	public Boolean getGenerico() {
+		return generico;
+	}
+
+	public void setGenerico(Boolean generico) {
+		this.generico = generico;
+	}
+	
+	
+}
